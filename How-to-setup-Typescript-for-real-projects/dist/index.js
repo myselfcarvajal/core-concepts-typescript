4b0a267f129057f499bbdf463dea9a5888ab2c82
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2024-03-21 17:14:18 -0500

    Object-oriented

--- How-to-setup-Typescript-for-real-projects/dist/index.js
+++ How-to-setup-Typescript-for-real-projects/dist/index.js
@@ -1,3 +1,44 @@
 "use strict";
-console.log("typescript is here");
-console.log("typescript is ");
+// class User {
+//   public email: string
+//   private name: string
+//   readonly city: string = "Tokyo"
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "Tokyo";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Error");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class subUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const hitesh = new User("gmail", "Luka");
+hitesh.name;
--- How-to-setup-Typescript-for-real-projects/src/index.ts
+++ How-to-setup-Typescript-for-real-projects/src/index.ts
@@ -1,2 +1,52 @@
-console.log("typescript is here")
-console.log("typescript is ")
+// class User {
+//   public email: string
+//   private name: string
+//   readonly city: string = "Tokyo"
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+class User {
+  protected _courseCount = 1
+
+  readonly city: string = "Tokyo"
+  constructor(
+    public email: string,
+    public name: string,
+    // private userId: string
+  ) {
+  }
+
+  private deleteToken() {
+    console.log("Token deleted")
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`
+  }
+
+  get courseCount(): number {
+    return this._courseCount
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Error");
+
+    }
+    this._courseCount = courseNum
+  }
+
+}
+
+class subUser extends User {
+  isFamily: boolean = true
+  changeCourseCount() {
+    this._courseCount = 4
+  }
+}
+
+const hitesh = new User("gmail", "Luka")
+hitesh.name
